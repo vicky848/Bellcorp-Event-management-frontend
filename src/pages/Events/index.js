fd0ddf4d653781512ff5f8c45154d327e2d3d352
vicky kumar <vickychowrasia35@gmail.com>
2026-02-15 23:20:33 +0530

    fix react hook dependency issue

--- src/pages/Events/index.js
+++ src/pages/Events/index.js
@@ -1,21 +1,20 @@
 import { useEffect, useState } from "react";
 import API from "../../services/api";
 import EventCard from "../../components/EventCard";
-import './index.css'
+import "./index.css";
 
 export default function Events() {
   const [events, setEvents] = useState([]);
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-     // eslint-disable-next-line
+    const fetchEvents = async () => {
+      const res = await API.get(`/events?search=${search}`);
+      setEvents(res.data);
+    };
+
     fetchEvents();
   }, [search]);
-
-  const fetchEvents = async () => {
-    const res = await API.get(`/events?search=${search}`);
-    setEvents(res.data);
-  };
 
   return (
     <div className="event-container">
